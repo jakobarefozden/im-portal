632d24d112391eb990cc993a155546426b32a5ed
Jakob Aref Øzden <jakobarefozden@gmail.com>
2025-11-29 00:51:46 +0100

    Set CORS allow-origin *

--- backend/main.go
+++ backend/main.go
@@ -71,7 +71,7 @@ func main() {
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Tüm origin'lere izin veriyoruz (içerik herkese açık olduğu için sorun yok)
+		// İçerik herkese açık olduğu için tüm originlere izin verebiliriz
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
